7f04c588877e81093d7622722ec3a7901d90e784
Rakshith Dharmappa <85234035+Rakshith2605@users.noreply.github.com>
2025-03-04 15:43:27 -0500

    structure change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Portfolio',
-  description: 'Portfolio of Rakshith Dharmappa',
+  title: 'Rakshith Dharmappa | Portfolio',
+  description: 'Portfolio of Rakshith Dharmappa, Data Scientist and HPC Engineer',
   generator: 'v0.dev',
 }
 
@@ -15,8 +15,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Add favicon */}
-        <link rel="icon" href="images/logo1.png?v=2" type="image/png" />
+        {/* Favicon */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        
+        {/* Viewport meta tag for responsive design */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+        {/* Additional meta tags for SEO and social media sharing */}
+        <meta name="author" content="Rakshith Dharmappa" />
+        <meta property="og:title" content="Rakshith Dharmappa | Portfolio" />
+        <meta property="og:description" content="Portfolio of Rakshith Dharmappa, Data Scientist and HPC Engineer" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://rakshith.info/" />
+        <meta property="og:image" content="https://rakshith.info//og-image.jpg" />
       </head>
       <body>
         {children}
